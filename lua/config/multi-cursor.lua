80cbdb3b51fce9797f5bb14cd8d6811df23c82c9
sohanemon <m.sohanemon@gmail.com>
2024-03-18 16:15:41 +0600

    refactor(multi-cursor.lua): updated key mappings for multi-cursor functionality

--- lua/config/multi-cursor.lua
+++ lua/config/multi-cursor.lua
@@ -9,12 +9,12 @@ local api = vim.api
 local c = require('vscode-multi-cursor')
 
 local k = vim.keymap.set
-k({ 'n', 'x' }, 'mC', c.create_cursor, { expr = true, desc = 'Create cursor' })
-k('n', 'mw', 'mCiw*<Cmd>nohl<CR>', { remap = true })
-k('n', 'mW', 'mCiw#<Cmd>nohl<CR>', { remap = true })
+k({ 'n', 'x' }, 'mulc', c.create_cursor, { expr = true, desc = 'Create cursor' })
+k('n', 'mw', 'mulciw*<Cmd>nohl<CR>', { remap = true })
+k('n', 'mW', 'mulciw#<Cmd>nohl<CR>', { remap = true })
 k({ 'n' }, 'mc', c.cancel, { desc = 'Cancel/Clear all cursors' })
 k({ 'n' }, 'mn', c.next_cursor, { desc = 'Goto next cursor' })
 k({ 'n' }, 'mN', c.prev_cursor, { desc = 'Goto prev cursor' })
-k({ 'v', 'x' }, 'L', c.selectHighlights, { desc = 'Select all match' })
+k({ 'x' }, 'L', c.selectHighlights, { desc = 'Select all match' })
 k({ 'n', 'x' }, 'I', c.start_left, { desc = 'Start cursors on the left' })
 k({ 'n', 'x' }, 'A', c.start_right, { desc = 'Start cursors on the right' })
